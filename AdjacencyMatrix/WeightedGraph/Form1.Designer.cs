391807593ff914d07069fa5e3da9d642171d50b8
Teeramate <teeramate.c@ku.th>
2024-02-22 16:18:27 +0700

    '22/2/2024'

---
+++ AdjacencyMatrix/WeightedGraph/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace WeightedGraph
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.srcComboBox = new System.Windows.Forms.ComboBox();
+            this.descComboBox = new System.Windows.Forms.ComboBox();
+            this.distanceTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.calButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Departure";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(33, 110);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Arrivals";
+            // 
+            // srcComboBox
+            // 
+            this.srcComboBox.FormattingEnabled = true;
+            this.srcComboBox.Location = new System.Drawing.Point(120, 25);
+            this.srcComboBox.Name = "srcComboBox";
+            this.srcComboBox.Size = new System.Drawing.Size(121, 24);
+            this.srcComboBox.TabIndex = 2;
+            // 
+            // descComboBox
+            // 
+            this.descComboBox.FormattingEnabled = true;
+            this.descComboBox.Location = new System.Drawing.Point(120, 110);
+            this.descComboBox.Name = "descComboBox";
+            this.descComboBox.Size = new System.Drawing.Size(121, 24);
+            this.descComboBox.TabIndex = 3;
+            // 
+            // distanceTextBox
+            // 
+            this.distanceTextBox.Location = new System.Drawing.Point(120, 200);
+            this.distanceTextBox.Name = "distanceTextBox";
+            this.distanceTextBox.Size = new System.Drawing.Size(121, 22);
+            this.distanceTextBox.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(33, 200);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "distance";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(267, 206);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(62, 16);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "kilometer";
+            // 
+            // calButton
+            // 
+            this.calButton.Location = new System.Drawing.Point(392, 60);
+            this.calButton.Name = "calButton";
+            this.calButton.Size = new System.Drawing.Size(123, 108);
+            this.calButton.TabIndex = 7;
+            this.calButton.Text = "distance_cal";
+            this.calButton.UseVisualStyleBackColor = true;
+            this.calButton.Click += new System.EventHandler(this.calButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(567, 255);
+            this.Controls.Add(this.calButton);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.distanceTextBox);
+            this.Controls.Add(this.descComboBox);
+            this.Controls.Add(this.srcComboBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "โปรแกรมคำนวณระยะทางระหว่างเมือง";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox srcComboBox;
+        private System.Windows.Forms.ComboBox descComboBox;
+        private System.Windows.Forms.TextBox distanceTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button calButton;
+    }
+}
+
---
+++ AdjacencyMatrix/WeightedGraph/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WeightedGraph
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+            srcComboBox.Items.AddRange(vertex_list);
+            srcComboBox.Text = vertex_list[0];
+            descComboBox.Items.AddRange(vertex_list);
+            descComboBox.Text = vertex_list[0];
+        }
+        string[] vertex_list = new string[] { "A", "B", "C", "D", "E","F" };
+        int[,] adj_list = new int[,] {{0,   523, 345, 0,   0,  0},
+                                      {523, 0,   200, 548, 0,  0},
+                                      {345, 200, 0,   360, 467,0},
+                                      {0,   548, 360, 245, 0,  0},
+                                      {0,   0,   467, 245, 0,  555},
+                                      {0,   0,   0,   320, 555,0}
+        };
+        private void calButton_Click(object sender, EventArgs e)
+        {
+            int src_index = srcComboBox.SelectedIndex;
+            int desc_index = descComboBox.SelectedIndex;
+            distanceTextBox.Text =
+            adj_list[src_index, desc_index].ToString();
+        }
+    }
+}
