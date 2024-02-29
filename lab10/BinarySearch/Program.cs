2bb94bd4a5f8deb71ee6e68833d54aa00ba0e41b
Teeramate <teeramate.c@ku.th>
2024-02-29 14:57:52 +0700

    Search

---
+++ lab10/BinarySearch/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearch
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            ArrayList mynums = new ArrayList(10);
+
+            for (int i = 0; i < 10; i++)
+            
+                mynums.Add(random.Next(10));
+                mynums.Sort();
+            
+            
+
+            int position = -1;
+            while (position == -1)
+            {
+                try
+                {
+                    Console.Write("Enter a number to search for: ");
+                    int searchNumber = int.Parse(Console.ReadLine());
+                    position = BinarySearch(mynums, searchNumber, 0, mynums.Count - 1);
+
+                    if (position > -1)
+                        Console.WriteLine("found item index=" + position);
+                    else
+                        Console.WriteLine("Not in the array");
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Invalid input. Please enter an integer.");
+                }
+
+                Console.Write("\nArray items: ");
+                for (int i = 0; i <= 9; i++)
+                {
+                    Console.Write(mynums[i] + " ");
+                }
+                Console.ReadLine();
+            }
+        }
+
+        static int BinarySearch(ArrayList arr, int value, int lower, int upper)
+        {
+            if (lower > upper)
+                return -1;
+            else
+            {
+                int mid;
+                mid = (int)(upper + lower) / 2;
+                if (value < (int)arr[mid])
+                    return BinarySearch(arr, value, lower, mid - 1);
+                else if (value == (int)arr[mid])
+                    return mid;
+                else
+                    return BinarySearch(arr, value, mid + 1, upper);
+            }
+        }
+    }
+}
+
---
+++ lab10/SequentialSearch/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SequentialSearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[5] { 1, 2, 3, 4, 5 };
+
+            Console.Write("Enter a number to search for: ");
+            int searchNumber = Convert.ToInt32(Console.ReadLine());
+
+            bool found = SeqSearch(numbers, searchNumber);
+
+            if (found)
+            {
+                Console.WriteLine($"{searchNumber} is in the array.");
+            }
+            else
+            {
+                Console.WriteLine($"{searchNumber} is not in the array.");
+            }
+        }
+
+        static bool SeqSearch(int[] arr, int sValue)
+        {
+            for (int index = 0; index < arr.Length; index++)
+            {
+                if (arr[index] == sValue)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
+
